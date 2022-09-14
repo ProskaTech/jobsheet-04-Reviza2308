dc3f3e258b789772293542d05d802354ba297a5c
Reviza2308 <112670494+Reviza2308@users.noreply.github.com>
2022-09-14 11:23:05 +0700

    Add files via upload

---
+++ js04/src/js04/satudua.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+    Created by : 21343031_Muhammad Reviza Dekry
+ */
+package js04;
+
+/**
+ *
+ * @author Revskyyy
+ */
+public class satudua {
+    public static void main(String[]args){
+        int score = 0;
+        char answer = 'a';
+        
+        score = (answer == 'b') ? 10 : 0;
+        System.out.println("answer = " +answer);
+}
+}
